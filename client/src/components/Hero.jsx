3cb4055d1514eaed03650191254d11fefa0d86f4
himanshukumar-2004 <himanshukr06092004@gmail.com>
2026-07-26 16:20:45 +0530

    feat: add Hero component with vehicle search form for location and date selection

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -7,8 +7,7 @@ const Hero = () => {
     return (
         <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
 
-            <h1 className='text-4xl md:text-5xl font-semibold'>Luxury cars on Rent by himanshu pal</h1>
-            angtod
+            <h1 className='text-4xl md:text-5xl font-semibold'>Luxury cars on Rent</h1>
             <form className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'>
 
                 <div className='flex flex-col md:flex-row items-start md:items-center gap-10 
