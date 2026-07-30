daaf5a0d5179425a36a1aaf1a1cd15a057aecbfe
himanshukumar-2004 <himanshukr06092004@gmail.com>
2026-07-30 17:36:13 +0530

    feat: initialize express server with database connection and core routes

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ await connectDB()
 
 //Middleware
 app.use(cors({
-	origin: ["https://rentyourdreamcar.vercel.app/", "http://localhost:5173"],
+	origin: ["https://rentyourdreamcar.vercel.app", "http://localhost:5173"],
 	credentials: true,
 }));
 app.use(express.json());
