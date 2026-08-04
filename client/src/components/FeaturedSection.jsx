6a830e272a4d5025c77312fa28b543f41a556b79
himanshukumar-2004 <himanshukr06092004@gmail.com>
2026-08-04 14:38:06 +0530

    feat: add Testimonial, Banner, FeaturedSection, and Newsletter components with motion animations

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Title from './Title'
-import { assets, dummyCarData } from '../assets/assets'
+import { assets } from '../assets/assets'
 import CarCard from './CarCard'
 import { useNavigate } from 'react-router-dom'
 import { useAppContext } from '../context/AppContext'
+import { motion } from 'motion/react'
 
 const FeaturedSection = () => {
 
@@ -11,34 +12,53 @@ const FeaturedSection = () => {
     const { cars } = useAppContext()
 
     return (
-        <div className='flex flex-col items-center py-24 px-6 md:px-16
+        <motion.div 
+        initial={{ opacity:0, y:40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className='flex flex-col items-center py-24 px-6 md:px-16
     lg:px-24 xl:px-32'>
 
-            <div>
+            <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay:0.3 }}
+            >
                 <Title title='Featured Vehicles' subTitle='Explore our selection of premium vehicles 
             available for your next adventure.'/>
-            </div>
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-12'>
+            </motion.div>
+            <motion.div 
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay:0.5 }}
+            className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-12'>
                 {
                     cars?.slice(0, 6).map((car) => (
-                        <div key={car._id}>
+                        <motion.div key={car._id}
+                          initial={{ opacity: 0, scale: 0.95 }}
+                          whileInView={{ opacity: 1, scale: 1 }}
+                          transition={{ duration: 0.4 , ease:"easeOut" }}
+                        >
                             <CarCard car={car} />
 
-                        </div>
+                        </motion.div>
                     ))
                 }
 
-            </div>
-            <button
+            </motion.div>
+            <motion.button
+             initial={{ opacity: 0, y: 20 }}
+             whileInView={{ opacity: 1, y: 0 }}
+             transition={{ duration: 0.4 , delay: 0.6 }}
                 onClick={() => {
                     navigate('/cars');
                     scrollTo(0, 0)
                 }}
                 className='flex items-center justify-center gap-2 px-6 py-2 border border-borderColor dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-800 rounded-md mt-6 cursor-pointer'>
                 Explore all cars<img src={assets.arrow_icon} alt="arrow" />
-            </button>
+            </motion.button>
 
-        </div>
+        </motion.div>
     )
 }
 
--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import {motion} from 'motion/react';
 
 const Newsletter = () => {
   return (
-   <div class="flex flex-col items-center justify-center text-center space-y-2
+   <motion.div 
+   initial={{opacity:0 , y: 30}}
+   whileInView={{ opacity:1, y: 0 }}
+   transition={{duration: 0.6, ease: 'easeOut'}}
+   viewport={{once: true, amount: 0.3}}
+   class="flex flex-col items-center justify-center text-center space-y-2
    max-md:px-4 my-10 mb-40">
-    <h1 class="md:text-4xl text-2xl font-semibold">Never Miss a Deal!</h1>
-    <p class="md:text-lg text-gray-500/70 dark:text-gray-400/70 pb-8">
+    <motion.h1 
+    initial={{ opacity:0, y: 20 }}
+    whileInView={{opacity:1, y: 0 }}
+    transition={{delay: 0.2, duration: 0.5}}
+    
+    class="md:text-4xl text-2xl font-semibold">Never Miss a Deal!</motion.h1>
+
+    <motion.p class="md:text-lg text-gray-500/70 dark:text-gray-400/70 pb-8">
         Subscribe to get the latest offers, new arrivals, and exclusive discounts
-    </p>
-    <form class="flex items-center justify-between max-w-2xl w-full md:h-13 h-12">
+    </motion.p>
+
+    <motion.form 
+    initial={{ opacity:0, y: 20 }}
+    whileInView={{opacity:1, y: 0 }}
+    transition={{delay: 0.2, duration: 0.5}}
+    class="flex items-center justify-between max-w-2xl w-full md:h-13 h-12">
         <input
             class="border border-gray-300 dark:border-gray-700 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500 dark:text-gray-300 dark:bg-gray-800"
             type="text"
@@ -18,8 +35,8 @@ const Newsletter = () => {
         <button type="submit" class="md:px-12 px-8 h-full text-white bg-primary hover:bg-primary-dull transition-all cursor-pointer rounded-md rounded-l-none">
             Subscribe
         </button>
-    </form>
-</div>
+    </motion.form>
+</motion.div>
   )
 }
 
