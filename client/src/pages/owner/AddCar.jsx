ac9ff6ae4769bfef578bd76cc5d8907f486e6201
himanshukumar-2004 <himanshukr06092004@gmail.com>
2026-08-14 07:41:05 +0530

    feat: add car listing form for owner dashboard

--- client/src/pages/owner/AddCar.jsx
+++ client/src/pages/owner/AddCar.jsx
@@ -234,7 +234,7 @@ const AddCar = () => {
             />
           </div>
 
-          <div className='flex justify-end w-full mt-4'>
+          <div className='flex justify-start w-full mt-1'>
             <button
               type='submit'
               className='bg-primary text-white px-4 py-2 rounded-md'
